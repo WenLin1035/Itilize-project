8a6d2662d03bb8ffd2e5dac9026da0e85258e5e0
Wenjuan Lin <wentatsulin1035@gmail.com>
2021-08-06 16:40:01 -0400

    modified entities based on new ERD

--- src/main/java/com/GroupProject/demo/Entity/Project.java
+++ src/main/java/com/GroupProject/demo/Entity/Project.java
@@ -1,37 +1,105 @@
 package com.GroupProject.demo.Entity;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
+
+/*
+@author: Wen Lin
+ */
 
 @Entity
 @Table(name="project")
 public class Project {
-    //primary key
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="project_code")
-    private int projectCode;
+    @GeneratedValue
+    @Column(name="project_id")
+    private Integer id;
 
-    @Column(name="project_name")
-    private String projectName;
+    @Column(name="Project_name",nullable = false,unique = true)
+    private String name;
 
-    public int getProjectCode() {
-        return projectCode;
+    @CreatedDate
+    @Column(name="createTime")
+    private LocalDate createtime;
+
+    @LastModifiedDate
+    @Column(name="updatetime")
+    private LocalDate updatetime;
+
+    @ManyToOne(targetEntity = User.class, cascade = {CascadeType.DETACH})
+    @JoinColumn(name="user_id")
+    private Integer user_id;
+
+    @Column(name="pr_id")
+    private  Integer pr_id;
+
+    @OneToMany(targetEntity = ProjectResource.class,cascade = CascadeType.REMOVE,mappedBy = "project")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private Set<ProjectResource> resources;
+
+    public Project(){}
+
+    public Project(Integer pid, String name, Integer uid, Integer pr_id){
+        id = pid;
+        this.name = name;
+        this.createtime = LocalDate.now();
+        this.updatetime = LocalDate.now();
+        user_id = uid;
+        this.pr_id = pr_id;
     }
 
-    public void setProjectCode(int projectCode) {
-        this.projectCode = projectCode;
+    public Integer getId(){
+        return id;
     }
 
-    public String getProjectName() {
-        return projectName;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
+    public String getName() {
+        return name;
     }
 
-    @Override
-    public String toString() {
-        return "Project [projectCode= " + projectCode + ", projectName= '" + projectName + "']";
+    public void setName(String name) {
+        this.name = name;
     }
+
+    public LocalDate getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(LocalDate createtime) {
+        this.createtime = createtime;
+    }
+
+    public LocalDate getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(LocalDate updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public Integer getPr_id() {
+        return pr_id;
+    }
+
+    public void setPr_id(Integer pr_id) {
+        this.pr_id = pr_id;
+    }
+
 }
