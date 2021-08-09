80d523ff3af84bc2fb404020f6ae8f1b30d6789f
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-09 03:15:03 -0400

    Creation of UserTests.java to test different scenarios related to users.

---
+++ src/test/java/com/GroupProject/demo/UserTests/UserTests.java
@@ -0,0 +1,18 @@
+package com.GroupProject.demo.UserTests;
+
+import com.GroupProject.demo.Entity.User;
+import org.junit.Before;
+
+public class UserTests {
+
+    private User user;
+
+    @Before
+    public void setup(){
+        user = new User();
+    }
+
+
+
+
+}
