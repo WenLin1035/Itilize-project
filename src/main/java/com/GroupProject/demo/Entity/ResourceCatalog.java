e76b4cf1a801205388c8e74add7dd2a8e29717d1
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-08 22:44:52 -0400

    ResourceCatalog entity to map ResourceCatalog table in database.

--- src/main/java/com/GroupProject/demo/Entity/ResourceCatalog.java
+++ src/main/java/com/GroupProject/demo/Entity/ResourceCatalog.java
@@ -1,6 +1,8 @@
 package com.GroupProject.demo.Entity;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name="resource_catalog")
@@ -9,18 +11,33 @@ public class ResourceCatalog {
     //primary key
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="resource_code")
-    private int resourceCode;
+    @Column(name="resource_id")
+    private int resourceId;
 
     @Column(name="resource_name")
     private String resourceName;
 
-    public int getResourceCode() {
-        return resourceCode;
+    @Column(name="resource_code")
+    private String resourceCode;
+
+    @Column(name="time_created")
+    private LocalDateTime timeCreated;
+
+    @Column(name="time_updated")
+    private LocalDateTime timeUpdated;
+
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="resourceCatalog", cascade= CascadeType.ALL)
+    private List<ProjectResources> projectResources;
+
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="resourceCatalog", cascade= CascadeType.ALL)
+    private List<Columns> columns;
+
+    public int getResourceId() {
+        return resourceId;
     }
 
-    public void setResourceCode(int resourceCode) {
-        this.resourceCode = resourceCode;
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
     }
 
     public String getResourceName() {
@@ -31,8 +48,56 @@ public class ResourceCatalog {
         this.resourceName = resourceName;
     }
 
+    public String getResourceCode() {
+        return resourceCode;
+    }
+
+    public void setResourceCode(String resourceCode) {
+        this.resourceCode = resourceCode;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDateTime getTimeUpdated() {
+        return timeUpdated;
+    }
+
+    public void setTimeUpdated(LocalDateTime timeUpdated) {
+        this.timeUpdated = timeUpdated;
+    }
+
+    public List<ProjectResources> getProjectResources() {
+        return projectResources;
+    }
+
+    public void setProjectResources(List<ProjectResources> projectResources) {
+        this.projectResources = projectResources;
+    }
+
+    public List<Columns> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(List<Columns> columns) {
+        this.columns = columns;
+    }
+
     @Override
     public String toString() {
-        return "ResourceCatalog [resourceCode= " + resourceCode + ", resourceName= '" + resourceName + "']";
+        return "ResourceCatalog{" +
+                "resourceId=" + resourceId +
+                ", resourceName='" + resourceName + '\'' +
+                ", resourceCode='" + resourceCode + '\'' +
+                ", timeCreated=" + timeCreated +
+                ", timeUpdated=" + timeUpdated +
+                ", projectResources=" + projectResources +
+                ", columns=" + columns +
+                '}';
     }
 }
