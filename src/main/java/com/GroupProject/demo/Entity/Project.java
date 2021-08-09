bce51962888b1325213cf742d054f9b0668935ed
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-08 22:51:29 -0400

    Adding getters and setters and overriding toString() method to Project.java class.

--- src/main/java/com/GroupProject/demo/Entity/Project.java
+++ src/main/java/com/GroupProject/demo/Entity/Project.java
@@ -2,6 +2,7 @@ package com.GroupProject.demo.Entity;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name="project")
@@ -25,5 +26,66 @@ public class Project {
     @JoinColumn(name="user_id")
     private User user;
 
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="resourceCatalog", cascade= CascadeType.ALL)
+    private List<ProjectResources> projectResources;
 
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDateTime getTimeUpdated() {
+        return timeUpdated;
+    }
+
+    public void setTimeUpdated(LocalDateTime timeUpdated) {
+        this.timeUpdated = timeUpdated;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<ProjectResources> getProjectResources() {
+        return projectResources;
+    }
+
+    public void setProjectResources(List<ProjectResources> projectResources) {
+        this.projectResources = projectResources;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "projectId=" + projectId +
+                ", projectName='" + projectName + '\'' +
+                ", timeCreated=" + timeCreated +
+                ", timeUpdated=" + timeUpdated +
+                ", user=" + user +
+                ", projectResources=" + projectResources +
+                '}';
+    }
 }
