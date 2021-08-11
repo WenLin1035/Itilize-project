ecc7fd35c335434d172d0f08e793713a278b4f10
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-11 14:16:04 -0400

    Rename ProjectResourcesRepository.java to ProjectResourceRepository.java.

--- src/main/java/com/GroupProject/demo/Repository/ProjectResourceRepository.java
+++ src/main/java/com/GroupProject/demo/Repository/ProjectResourceRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectResourcesRepository extends JpaRepository<ProjectResources, Integer> {
+public interface ProjectResourceRepository extends JpaRepository<ProjectResources, Integer> {
 }
