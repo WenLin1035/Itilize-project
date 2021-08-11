2b2c950d138214a2122117e8ecc841ab3e317020
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-11 13:08:01 -0400

    Renaming ColumnsRepository.java to ColumnRepository.java.

--- src/main/java/com/GroupProject/demo/Repository/ColumnRepository.java
+++ src/main/java/com/GroupProject/demo/Repository/ColumnRepository.java
@@ -3,5 +3,5 @@ package com.GroupProject.demo.Repository;
 import com.GroupProject.demo.Entity.Columns;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ColumnsRepository extends JpaRepository<Columns, Integer> {
+public interface ColumnRepository extends JpaRepository<Columns, Integer> {
 }
