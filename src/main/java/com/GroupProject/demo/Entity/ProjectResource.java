015db5636917a4f43fd25f3b43b09a120d76e002
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-11 15:08:14 -0400

    Change table entity name from "project_resources" to "project_resource" and adding timeUpdated and timeCreated variables to track changes.

--- src/main/java/com/GroupProject/demo/Entity/ProjectResource.java
+++ src/main/java/com/GroupProject/demo/Entity/ProjectResource.java
@@ -1,9 +1,10 @@
 package com.GroupProject.demo.Entity;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name="project_resources")
+@Table(name="project_resource")
 public class ProjectResource {
 
     @Id
@@ -18,6 +19,12 @@ public class ProjectResource {
     @ManyToOne(cascade= {CascadeType.DETACH}, targetEntity = Resource.class)
     @JoinColumn(name="resource_id")
     private Resource resource;
+
+    @Column(name="time_created")
+    private LocalDateTime timeCreated;
+
+    @Column(name="time_updated")
+    private LocalDateTime timeUpdated;
 
     public Integer getPR_id() {
         return PR_id;
@@ -35,12 +42,28 @@ public class ProjectResource {
         this.project = project;
     }
 
-    public Resource getResources() {
+    public Resource getResource() {
         return resource;
     }
 
-    public void setResourceCatalog(Resource resource) {
+    public void setResource(Resource resource) {
         this.resource = resource;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDateTime getTimeUpdated() {
+        return timeUpdated;
+    }
+
+    public void setTimeUpdated(LocalDateTime timeUpdated) {
+        this.timeUpdated = timeUpdated;
     }
 
     @Override
