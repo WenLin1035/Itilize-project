67d54740ae756bf1f70a1cd1b53102e10282be8e
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-13 03:51:20 -0400

    Adding simple "korera/users" api to get all users.

--- src/main/java/com/GroupProject/demo/Controller/UserController.java
+++ src/main/java/com/GroupProject/demo/Controller/UserController.java
@@ -1,7 +1,26 @@
 package com.GroupProject.demo.Controller;
 
-import org.springframework.stereotype.Controller;
+import com.GroupProject.demo.Entity.User;
+import com.GroupProject.demo.Service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/korera")
 public class UserController {
+
+    private  final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok().body(userService.getAlUsers());
+    }
 }
