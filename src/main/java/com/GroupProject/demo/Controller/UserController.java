950760674af86d5cac74de7059b783a87a327a6d
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-08-13 04:28:05 -0400

    Adding another "korera/user/create" api to handle user creation/sign up.

--- src/main/java/com/GroupProject/demo/Controller/UserController.java
+++ src/main/java/com/GroupProject/demo/Controller/UserController.java
@@ -1,26 +1,35 @@
 package com.GroupProject.demo.Controller;
 
 import com.GroupProject.demo.Entity.User;
+import com.GroupProject.demo.Service.ServiceImplementation.UserServiceImpl;
 import com.GroupProject.demo.Service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/korera")
 public class UserController {
 
-    private  final UserService userService;
+    private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers(){
-        return ResponseEntity.ok().body(userService.getAlUsers());
+        URI uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/korera/user/create")
+                .toUriString());
+        return ResponseEntity.created(uri).body(userService.getAlUsers());
+    }
+
+    @PostMapping("/user/signup")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return ResponseEntity.ok().body(userService.createUser(user));
     }
 }
